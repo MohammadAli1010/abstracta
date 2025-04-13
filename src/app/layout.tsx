2b244b578e47f767a901fb5885bc519fc4ccb2ef
MohammadAli1010 <mohammadali991001@gmail.com>
2025-04-13 15:36:24 +0530

    Header and Global Styling stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight: ['200','300','400','500','600','700','800','900'],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Abstracta - AI-Powered PDF Summarization",
-  description: "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
 };
 
 export default function RootLayout({
@@ -20,10 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} fontSans antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} fontSans antialiased`}>
+        <div className="relative flex min-h-screen flex-col">
+          <Header></Header>
+          <main className="flex-1">{children}</main>
+          <Footer></Footer>
+        </div>
       </body>
     </html>
   );
