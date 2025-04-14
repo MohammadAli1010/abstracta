afec9ba37298d189575515b2ecb94c302fc907d8
MohammadAli1010 <mohammadali991001@gmail.com>
2025-04-14 19:33:13 +0530

    finished the hero section and how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 // import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/home/hero-section";
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
     <div className=" relative w-full">
-      <HeroSection></HeroSection>
-      {/* <DemoSection></DemoSection> */}
-      {/* <HowItWorksSection></HowItWorksSection> */}
-      {/* <PricingSection></PricingSection> */}
+      <BgGradient></BgGradient>
+      <div className="flex flex-col">
+        <HeroSection></HeroSection>
+        <DemoSection></DemoSection>
+        <HowItWorksSection></HowItWorksSection>
+        <PricingSection></PricingSection>
+      </div>
+
       {/* <CTASection></CTASection> */}
     </div>
   );
--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -7,17 +7,16 @@ import { ArrowRight } from "lucide-react";
 export default function HeroSection() {
   return (
     <section className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 transition-all animate-in lg:px-12 max-w-7xl">
-      <div className="">
-        <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
-          <Badge
-            variant={"secondary"}
-            className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
-          >
-            <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"></Sparkles>
-            <p className="text-base text-rose-600">Powered by AI</p>
-          </Badge>
-        </div>
+      <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
+        <Badge
+          variant={"secondary"}
+          className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
+        >
+          <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"></Sparkles>
+          <p className="text-base text-rose-600">Powered by AI</p>
+        </Badge>
       </div>
+
       <h1 className="font-bold py-6 text-center">
         Transform PDFs into{" "}
         <span className="relative inline-block">
@@ -35,7 +34,7 @@ export default function HeroSection() {
       <div>
         <Button
           variant={"link"}
-          className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg     transition-all duration-300"
+          className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg transition-all duration-300"
         >
           <Link href="/#pricing" className="flex gap-2 items-center">
             <span>Try Abstracta</span>
