6fd55b970ddcc757408e44f800d78927f8a3d418
MohammadAli1010 <mohammadali991001@gmail.com>
2025-04-16 14:35:25 +0530

    reverted gradient to linear

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -7,7 +7,7 @@ import { ArrowRight } from "lucide-react";
 export default function HeroSection() {
   return (
     <section className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 transition-all animate-in lg:px-12 max-w-7xl">
-      <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
+      <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
         <Badge
           variant={"secondary"}
           className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
