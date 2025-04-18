8db99ddfa39f88e39dfdff3a93e0fce6baf3f278
MohammadAli1010 <mohammadali991001@gmail.com>
2025-04-18 15:24:14 +0530

    got the toaster noti on top right

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,10 +10,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-right"
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
+          "--normal-bg": "palevioletred", // A slightly darker shade of pink
+          "--normal-text": "rose",
           "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
