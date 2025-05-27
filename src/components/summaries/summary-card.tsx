0ba3389e8cf538af2730964a58636b8a554c6b9a
Mohammad Ali <mohammadali991001@gmail.com>
2025-05-27 10:25:34 +0530

    Starting on retrieving data to show in description

--- src/components/summaries/summary-card.tsx
+++ src/components/summaries/summary-card.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Card } from "../ui/card";
 import DeleteButton from "./delete-button";
 import { FileText } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const SummaryHeader = ({
   fileUrl,
@@ -19,9 +20,24 @@ const SummaryHeader = ({
         <h3 className="text-base xl:text-lg font-semibold text-gray-900 truncate w-4/5">
           {title}
         </h3>
-        <p className="text-sm text-gray-500">2025</p>
+        <p className="text-sm text-gray-500">{createdAt}</p>
       </div>
     </div>
+  );
+};
+
+const StatusBadge = ({ status }: { status: string }) => {
+  return (
+    <span
+      className={cn(
+        "px-3 py-1 text-xs font-medium rounded-full capitalize",
+        status === "completed"
+          ? "bg-green-100 text-green-800"
+          : "bg-yellow-100 text-yellow-800"
+      )}
+    >
+      {status}
+    </span>
   );
 };
 
@@ -33,17 +49,19 @@ export default function SummaryCard({ summary }: { summary: any }) {
           <DeleteButton />
         </div>
         <Link href={`summaries/${summary.id}`} className="block p-4 sm:p-6">
-          <SummaryHeader
-            fileUrl={summary.original_file_url}
-            title={summary.title}
-            createdAt={summary.created_at}
-          />
-          <p className="text-gray-600 line-clamp-2 text-sm sm:text-base pl-2">
-            {summary.summary_text}
-          </p>
+          <div className="flex flex-col gap-3 sm:gap-4">
+            <SummaryHeader
+              fileUrl={summary.original_file_url}
+              title={summary.title}
+              createdAt={summary.created_at}
+            />
+            <p className="text-gray-600 line-clamp-2 text-sm sm:text-base pl-2">
+              {summary.summary_text}
+            </p>
 
-          <div>
-            <span></span>
+            <div className="flex justify-between items-center mt-2 sm:mt-4">
+              <StatusBadge status={summary.status} />
+            </div>
           </div>
         </Link>
       </Card>
