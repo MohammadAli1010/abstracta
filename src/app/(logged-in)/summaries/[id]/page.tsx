269ca0799973a10b3cb0fbd0c93a3fcb8c875a2f
Mohammad Ali <mohammadali991001@gmail.com>
2025-05-29 09:41:13 +0530

    working on summary id

--- src/app/(logged-in)/summaries/[id]/page.tsx
+++ src/app/(logged-in)/summaries/[id]/page.tsx
@@ -1,3 +1,12 @@
-export default function SummaryPage() {
-  return <div>SummaryPage</div>;
+import { getSummaryById } from "@/components/summaries/summary";
+
+export default async function SummaryPage(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const params = await props.params;
+  const id = params.id;
+
+  const summary = await getSummaryById(id);
+
+  return <div>SummaryPage {id}</div>;
 }
