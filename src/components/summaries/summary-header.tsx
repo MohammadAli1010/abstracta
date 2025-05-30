c5c9708480602cf9c32cba2f4684328bf7cec414
Mohammad Ali <mohammadali991001@gmail.com>
2025-05-30 09:03:01 +0530

    working on summary page header

--- src/components/summaries/summary-header.tsx
+++ src/components/summaries/summary-header.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 export function SummaryHeader({ title }: { title: string }) {
-  return <h1>{title}</h1>;
+  return (
+    <div className="flex gap-4 mb-4 justify-between">
+      <div></div>
+      <div>
+        <Link href="/dashboard">
+          <Button
+            variant={"link"}
+            size="sm"
+            className="group flex items-center gap-1 sm:gap-2 hover:bg-white/80 backdrop-blur-xs rounded-full transition-all duration-200 shadow-xs hover:shadow-md border border-rose-100/30 bg-rose-100 px-2 sm:px-3"
+          >
+            Back to Dashboard
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
