1f1097e656ff412c6daffe2b69ef3115ad21588c
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-01 08:56:00 +0530

    Adding Stripe

---
+++ src/app/api/payments/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    return NextResponse.json({
+        status: 'success',
+        message: 'Hello from Stripe API',
+    });
+};
