1900be00801e732d4c22e96db913de3e1d6a04c9
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-03 14:49:05 +0530

    working on payments

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -1,3 +1,4 @@
+import { handleCheckoutSessionCompleted } from "@/lib/payments";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from 'stripe';
 
@@ -20,8 +21,13 @@ export const POST= async (req: NextRequest) => {
         switch (event.type){
             case 'checkout.session.completed':
                 console.log('Checkout session completed');
-                const session = event.data.object;
-                console.log(session);
+                const sessionId = event.data.object.id;
+                const session = await stripe.checkout.sessions.retrieve(sessionId, { expand: ['line_items'],
+
+                });
+
+                await handleCheckoutSessionCompleted({session, stripe});
+                
                 break;
             case 'customer.subscription.deleted':
                 console.log('Customer subscription deleted');
---
+++ src/lib/payments.ts
@@ -0,0 +1,50 @@
+import { getDbConnection } from "./db";
+import Stripe from "stripe";
+
+export async function handleCheckoutSessionCompleted({
+    session, stripe,
+}: {
+    session: Stripe.Checkout.Session;
+    stripe: Stripe;
+}) {
+    console.log('Checkout session completed', session);
+    const customerId = session.customer as string;
+    const customer = await stripe.customers.retrieve(customerId);
+    const priceId = session.line_items?.data[0]?.price?.id;
+
+    if ('email' in customer && priceId) {
+        const { email, name} = customer;
+
+        await createOrUpdateUser({
+            email: email as string,
+            fullName: name as string,
+            customerId,
+            priceId: priceId as string,
+            status: 'active',
+        });
+    }
+}
+
+async function createOrUpdateUser({
+    email,
+    fullName,
+    customerId,
+    priceId,
+    status,
+}: {
+    email: string;
+    fullName: string;
+    customerId: string;
+    priceId: string;
+    status: string;
+}) {
+    try {
+        const sql = await getDbConnection();
+        const user = await sql`SELECT * FROM users WHERE email= ${email}`;
+        if (user.length === 0) {
+            await sql`INSERT INTO users (email,full_name, customer_id, price_id, status) VALUES (${email}, ${fullName}, ${customerId}, ${priceId}, ${status})`;
+        }
+    } catch (error) {
+        console.error('Error creating or updating users', error);
+    }
+}
