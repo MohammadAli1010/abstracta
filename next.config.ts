3bdf8b4992380aff4770511aa41494448ec83b00
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-03 14:30:15 +0530

    Tested with LocalTunnel (Working)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  allowedDevOrigins: ["https://abstracta.loca.lt"],
 };
 
 export default nextConfig;
