60f218db2ad564313a1bb4c95d6ae97d1b491375
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-05 16:28:14 +0530

    done with adding all the motion to the pages

--- src/app/(logged-in)/upload/page.tsx
+++ src/app/(logged-in)/upload/page.tsx
@@ -4,6 +4,8 @@ import UploadForm from "@/components/upload/upload-form";
 import { currentUser } from "@clerk/nextjs/server";
 import { hasReachedUploadLimit } from "@/lib/user";
 import { redirect } from "next/navigation";
+import { MotionDiv } from "@/components/common/motion-wrapper";
+import { containerVariants } from "../../../../utils/constants";
 
 export default async function Page() {
   const user = await currentUser();
@@ -22,12 +24,17 @@ export default async function Page() {
   return (
     <section className="min-h-screen">
       <BgGradient></BgGradient>
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <MotionDiv
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8"
+      >
         <div className="flex flex-col items-center justify-center gap-6 text-center">
           <UploadHeader></UploadHeader>
           <UploadForm></UploadForm>
         </div>
-      </div>
+      </MotionDiv>
     </section>
   );
 }
