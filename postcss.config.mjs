6c17191d0037e1642e95d4b2d15d9f9e32e027a4
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-07 23:45:55 +0530

    fixing postcss err prt 2

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,9 +3,20 @@
 // };
 
 // export default config;
+
+// import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
+
+// const config = {
+//   plugins: [tailwindcss, autoprefixer],
+// };
+
+// export default config;
+
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [tailwindcss, autoprefixer],
 };
