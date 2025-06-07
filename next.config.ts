ec418f016342b50e5e3f9b70345e2fffe69da2ff
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-07 23:37:25 +0530

    fixing src issues

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+  
+//   allowedDevOrigins: ["https://abstracta.loca.lt"],
+// };
+
+// export default nextConfig;
+
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  // Your existing config
   allowedDevOrigins: ["https://abstracta.loca.lt"],
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
