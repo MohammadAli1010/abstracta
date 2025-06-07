ec00b11c953f594a3ba9b9e110b36a11247fab6f
Mohammad Ali <mohammadali991001@gmail.com>
2025-06-07 23:49:44 +0530

    fixing take 3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -13,12 +13,20 @@
 
 // export default config;
 
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+// import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
 
-/** @type {import('postcss-load-config').Config} */
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: [tailwindcss, autoprefixer],
+// };
+
+// export default config;
+
+import tailwindcss from "tailwindcss";
+
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss()],
 };
 
 export default config;
